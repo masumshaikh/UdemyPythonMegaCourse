77e663042bff91e6843b9c4908e448fc1f5b9d53
masumshaikh <smshaikh@hotmail.com>
2019-03-25 15:56:40 +0000

    render about page in try_flask.py

--- 007 App4 Build a website with Flask/try_flask.py
+++ 007 App4 Build a website with Flask/try_flask.py
@@ -5,13 +5,17 @@ Created on Sun Mar 17 22:58:00 2019
 @author: e1081018
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/') # this means the page is the home page
 def home():
-    return "Can't believe I spent money to learn how to do this!"
+    return render_template("home.html")
+
+@app.route('/about/') # this means the page is in the /about/ directory
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
