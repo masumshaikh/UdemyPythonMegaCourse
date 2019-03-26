10c75885f2e2230daf1eac4237184e8abb3de841
masumshaikh <smshaikh@hotmail.com>
2019-03-26 14:30:36 +0000

    Support proxies in Slow German script

--- 005 webscraping with beautiful soup/SlowGerman.py
+++ 005 webscraping with beautiful soup/SlowGerman.py
@@ -13,14 +13,14 @@ def get_soup(url, proxies = None):
     r = requests.get(url, proxies = proxies)
     return Soup(r.content, 'html.parser')
 
-def get_all_url_links():
+def get_all_url_links(proxies = None):
     url = "https://slowgerman.com/inhaltsverzeichnis/"
-    soup = get_soup(url)
+    soup = get_soup(url, proxies)
     all_links = soup.find("ul", {"class":"lcp_catlist"}).find_all("li")
     return [link for link in all_links if "SG #" in link.text]
 
-def create_dicts():
-    all_link_tags = get_all_url_links()
+def create_dicts(proxies = None):
+    all_link_tags = get_all_url_links(proxies)
     all_links = [tag.find("a")['href'] for tag in all_link_tags]
     all_titles = [link.text.strip() for link in all_link_tags]
     indices = [int(re.search(r"(?<=#).*?(?=\:)", title).group()) for title in all_titles]
@@ -30,8 +30,8 @@ def create_dicts():
         dict_titles = {indices[i]:all_titles[i] for i in range(len(all_links))}        
         return (dict_links, dict_titles)
 
-def get_transcript(url):
-    soup = get_soup(url)
+def get_transcript(url, proxies = None):
+    soup = get_soup(url, proxies)
     temp = (soup.find("div", {"class":"entry-content"})).text
     for i in range(5):
         temp = re.sub(r"\n\n", r"\n", temp)
@@ -42,10 +42,11 @@ def get_filename(title):
     temp = re.sub(r"SG #(\d+): ", r"SG\1-", title) + ".txt"
     temp = re.sub("/", "or", temp)
     return temp
-    
-dict_links, dict_titles = create_dicts()    
-for i in range(170,180):
+
+proxies = {'http': 'http://localhost:3128', 'https': 'https://localhost:3128'}
+dict_links, dict_titles = create_dicts(proxies)    
+for i in range(180,182):
     print(get_filename(dict_titles[i]))
     with open(get_filename(dict_titles[i]), "w") as the_file:
         the_file.write(dict_titles[i])
-        the_file.write(get_transcript(dict_links[i]))
+        the_file.write(get_transcript(dict_links[i], proxies))
